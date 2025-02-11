8b2c176aa53354fa2780bbef8c99cf366f29a027
jackpress <jackpress2027@u.northwestern.edu>
2025-02-11 11:46:48 -0600

    rearranging

---
+++ Misc/leg_testing.py
@@ -0,0 +1,9 @@
+import time
+import utility
+
+if __name__ == "__main__":
+    utility.reset_legs()
+    
+    utility.walk_forward()
+
+    utility.reset_legs()
--- asn0_f/leg_testing.py
+++
@@ -1,9 +0,0 @@
-import time
-import utility
-
-if __name__ == "__main__":
-    utility.reset_legs()
-    
-    utility.walk_forward()
-
-    utility.reset_legs()
